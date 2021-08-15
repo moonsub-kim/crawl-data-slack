02c1deecfd5bcdcae6ec46e7972a7b5bb80476b1
mskim-raf <raf.kim@buzzvil.com>
2021-08-15 15:08:22 +0900

    ignore empty record

--- internal/pkg/groupwaredecline/crawler.go
+++ internal/pkg/groupwaredecline/crawler.go
@@ -75,6 +75,10 @@ func (c Crawler) Crawl() ([]crawler.Event, error) {
 			function crawl() {
 				var trs = document.body.querySelectorAll('div.grid-content > table > tbody > tr');
 				var records = [];
+				if (trs.length == 1) {
+					return "[]"
+				}
+
 				for (var i = 0; i < trs.length; i++) {
 					var arr = [];
 					var tds = trs[i].getElementsByTagName('td');
