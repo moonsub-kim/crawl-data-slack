0cd7f791be4acbe0ffe0f9a20062dde3c85b6740
mskim-raf <raf.kim@buzzvil.com>
2021-09-12 16:04:45 +0900

    refactoring

--- internal/pkg/hackernews/builder.go
+++ internal/pkg/hackernews/builder.go
@@ -2,7 +2,6 @@ package hackernews
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 
 	"github.com/moonsub-kim/crawl-data-slack/internal/pkg/crawler"
@@ -11,26 +10,29 @@ import (
 type eventBuilder struct {
 }
 
+func (b eventBuilder) filter(filters []Filter, subText string) (string, string, bool) {
+	var parsed string
+	for _, f := range filters {
+		if f.Filter(subText) {
+			return f.Reason(), "", true
+		}
+		parsed += f.String() + " "
+	}
+
+	return "", strings.TrimSpace(parsed), false
+}
+
 func (b eventBuilder) buildEvents(dtos []DTO, crawlerName, jobName string, channel string) ([]crawler.Event, error) {
 	var events []crawler.Event
+	filters := []Filter{&adFilter{}, &ageFilter{}, &pointFilter{}, &commentFilter{}}
+
 	for _, dto := range dtos {
 		// 292 points by geox 16 hours ago | hide | 140 comments
 		// 49 minutes ago | hide
 		// 11 points by todsacerdoti 46 minutes ago | hide | discuss
-
-		age := regexp.MustCompile(`\d+ [A-z]+ ago`).FindString(dto.SubText)
-		if strings.Contains(age, "minute") {
-			fmt.Printf("ignore recent 1h post %v\n", dto)
-			continue
-		} else if !strings.Contains(dto.SubText, "comment") && !strings.Contains(dto.SubText, "discuss") {
-			fmt.Printf("ignore ad post %v\n", dto)
-			continue
-		}
-
-		comments := regexp.MustCompile(`\d+.comments?`).FindString(dto.SubText)
-		comments = strings.ReplaceAll(comments, "&nbsp;", " ")
-		if comments == "" {
-			comments = "discuss"
+		reason, subText, filtered := b.filter(filters, dto.SubText)
+		if filtered {
+			fmt.Printf("%s %v\n", reason, dto)
 		}
 
 		events = append(
@@ -42,12 +44,11 @@ func (b eventBuilder) buildEvents(dtos []DTO, crawlerName, jobName string, chann
 				UID:      dto.ID,
 				Name:     "news",
 				Message: fmt.Sprintf(
-					"<%s|%s>\n(%s, <%s|%s>)",
+					"<%s|%s>\n(%s|%s>)",
 					dto.URL,
 					dto.Title,
-					age,
 					dto.CommentURL,
-					comments,
+					subText,
 				),
 			},
 		)
---
+++ internal/pkg/hackernews/filter.go
@@ -0,0 +1,99 @@
+package hackernews
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Filter interface {
+	Filter(subText string) bool
+	Reason() string
+	String() string
+}
+
+type filter struct {
+	reason string
+	parsed string
+}
+
+func (f *filter) Reason() string {
+	return f.reason
+}
+
+func (f *filter) String() string {
+	return f.parsed
+}
+
+type adFilter struct {
+	filter
+}
+
+func (f *adFilter) Filter(subText string) bool {
+	if !strings.Contains(subText, "comment") && !strings.Contains(subText, "discuss") {
+		f.reason = "advertisement filter"
+		return true
+	}
+
+	return false
+}
+
+type ageFilter struct {
+	filter
+}
+
+func (f *ageFilter) Filter(subText string) bool {
+	age := regexp.MustCompile(`(\d+) [A-z]+ ago`).FindStringSubmatch(subText)
+	if len(age) != 2 {
+		f.reason = "age is not matched"
+		return true
+	} else if strings.Contains(age[0], "minute") {
+		f.reason = "ignore recent 1h post"
+		return true
+	} else if v, err := strconv.Atoi(age[1]); err != nil {
+		f.reason = fmt.Sprintf("Atoi error %v", err)
+		return true
+	} else if strings.Contains(age[0], "hour") && v < 2 {
+		f.reason = "ignore recent 1h post"
+		return true
+	}
+	f.parsed = age[0]
+
+	return false
+}
+
+type pointFilter struct {
+	filter
+}
+
+func (f *pointFilter) Filter(subText string) bool {
+	point := regexp.MustCompile(`(\d+) poins?t`).FindStringSubmatch(subText)
+	if len(point) != 2 {
+		f.reason = "point is not matched"
+		return true
+	} else if v, err := strconv.Atoi(point[1]); err != nil {
+		f.reason = fmt.Sprintf("Atoi error %v", err)
+		return true
+	} else if v < 20 {
+		f.reason = "ignore under less than point"
+		return true
+	}
+	f.parsed = point[0]
+
+	return false
+}
+
+type commentFilter struct {
+	filter
+}
+
+func (f *commentFilter) Filter(subText string) bool {
+	comments := regexp.MustCompile(`\d+.comments?`).FindString(subText)
+	f.parsed = comments
+	if comments == "" {
+		f.parsed = "discuss"
+	}
+
+	return false
+}
